ecdbe7e858a3af0e2cd8f4ef5c3cb5ea58218911
Daniel <danisaifudinov@gmail.com>
2026-06-19 00:15:17 +0600

    Comment out SECURE_SSL_REDIRECT in base.py
    Comment out SECURE_SSL_REDIRECT setting in production.

--- config/settings/base.py
+++ config/settings/base.py
@@ -234,7 +234,7 @@ LOGGING = {
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+   # SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
